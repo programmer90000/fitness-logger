f09006a2f5d020cfe3f15921010f16000def705a
Abdul Rehman <abdulr568programming@outlook.com>
2024-09-03 16:48:34 +0100

    config: Created tailwind file

---
+++ tailwind.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    content: ["./ App.{ js, jsx, ts, tsx }”, “./ components/**/ *.{ js, jsx, ts, tsx }"], // Add any other directories containing code which will be displayed, such as the screens directory
+    plugins: [], // Require any Tailwind CSS plugins I want to use, such as `require(“@tailwindcss/forms”)
+};

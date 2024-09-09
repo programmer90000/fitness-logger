41d5d09ee35716eae36d16b9fdf899b9e4c203af
Abdul Rehman <abdulr568programming@outlook.com>
2024-09-09 13:22:06 +0100

    feat: Made each icon a link

--- app/components/Footer/Footer.js
+++ app/components/Footer/Footer.js
@@ -1,5 +1,6 @@
 import { View } from "react-native";
 import { styles } from "./style.js";
+import { Link } from "expo-router";
 
 import Entypo from "@expo/vector-icons/Entypo";
 import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
@@ -10,11 +11,11 @@ import Feather from "@expo/vector-icons/Feather";
 export default function Footer() {
     return (
         <View style = {styles.container}>
-            <Entypo name = "home" size = {24} color = "black" />
-            <FontAwesome5 name = "history" size = {24} color = "black" />
-            <AntDesign name = "plussquare" size = {24} color = "black" />
-            <MaterialCommunityIcons name = "chart-line" size = {24} color = "black" />            
-            <Feather name = "settings" size = {24} color = "black" />
+            <Link href = "/"><Entypo name = "home" size = {24} color = "black" /></Link>
+            <Link href = "/"><FontAwesome5 name = "history" size = {24} color = "black" /></Link>
+            <Link href = "/"><AntDesign name = "plussquare" size = {24} color = "black" /></Link>
+            <Link href = "/"><MaterialCommunityIcons name = "chart-line" size = {24} color = "black" /></Link>            
+            <Link href = "/"><Feather name = "settings" size = {24} color = "black" /></Link>
         </View>
     );
 }

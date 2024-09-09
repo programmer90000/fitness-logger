e2bfa7de727832ae1031266e3240b5db6d5b5232
Abdul Rehman <127865680+programmer90000@users.noreply.github.com>
2024-09-09 12:00:49 +0100

    merge: Update docs/installing-software with main branch (#9)

---
+++ app/_layout.js
@@ -0,0 +1,12 @@
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { Drawer } from "expo-router/drawer";
+
+export default function Layout() {
+    return (
+        <GestureHandlerRootView style = {{ "flex": 1 }}>
+            <Drawer screenOptions = {{ "headerStyle": { "backgroundColor": "#FF0000" }, "drawerStyle": { "backgroundColor": "#FF0000" }, "drawerActiveTintColor": "#F1F1F1", "drawerActiveBackgroundColor": "transparent", "drawerInactiveTintColor": "#060606", "drawerInactiveBackgroundColor": "transparent" }}>
+                <Drawer.Screen name = "index" options = {{ "drawerLabel": "Home", "title": "Home" }} />
+            </Drawer>
+        </GestureHandlerRootView>
+    );
+}

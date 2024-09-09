0569e0f166b2db2dd5efe2202056dffbe0be6f0e
Abdul Rehman <abdulr568programming@outlook.com>
2024-09-09 09:51:08 +0100

    other: Seperated CSS from index.js into new stylesheet

--- app/index.js
+++ app/index.js
@@ -1,14 +1,6 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-
-const styles = StyleSheet.create({
-    "container": {
-        "flex": 1,
-        "backgroundColor": "#fff",
-        "alignItems": "center",
-        "justifyContent": "center",
-    },
-});
+import { Text, View } from "react-native";
+import { styles } from "./style.js";
 
 export default function App() {
     return (

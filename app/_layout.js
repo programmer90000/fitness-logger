9b5c5517542065c97724c1f7e2efb60adcdf643f
Abdul Rehman <abdulr568programming@outlook.com>
2024-09-17 16:44:22 +0100

    screen: Added Create A New Workout screen

--- app/_layout.js
+++ app/_layout.js
@@ -6,6 +6,7 @@ export default function Layout() {
         <GestureHandlerRootView style = {{ "flex": 1 }}>
             <Drawer screenOptions = {{ "headerStyle": { "backgroundColor": "#FF0000" }, "drawerStyle": { "backgroundColor": "#FF0000" }, "drawerActiveTintColor": "#F1F1F1", "drawerActiveBackgroundColor": "transparent", "drawerInactiveTintColor": "#060606", "drawerInactiveBackgroundColor": "transparent" }}>
                 <Drawer.Screen name = "index" options = {{ "drawerLabel": "Home", "title": "Home" }} />
+                <Drawer.Screen name = "components/create-a-new-workout-preset/create-a-new-workout-preset" options = {{ "drawerLabel": "Create A New Workout", "title": "Create A New Workout" }} />
             </Drawer>
         </GestureHandlerRootView>
     );

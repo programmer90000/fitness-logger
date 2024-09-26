e7dd2f118bd918611f977c5dd362c735c36e4cfe
Abdul Rehman <abdulr568programming@outlook.com>
2024-09-26 12:17:56 +0100

    fix: Fixed linting issues

--- app/components/create-a-new-workout-preset/create-a-new-workout-preset.js
+++ app/components/create-a-new-workout-preset/create-a-new-workout-preset.js
@@ -49,79 +49,79 @@ const WorkoutForm = () => {
     };
 
     return (
-        <ScrollView style={{ backgroundColor: colours.white }}>
-            <View className="items-center m-[5px]">
-                <Text style = {{color: colours.black }} className="text-xl">Workout Name</Text>
+        <ScrollView style = {{ "backgroundColor": colours.white }}>
+            <View className = "items-center m-[5px]">
+                <Text style = {{ "color": colours.black }} className = "text-xl">Workout Name</Text>
                 <Controller
                     control = {control}
                     name = "workoutName"
                     render = {({ "field": { onChange, onBlur, value } }) => { return (
-                        <TextInput onBlur = {onBlur} onChangeText = {onChange} value = {value} className="align-middle text-center w-11/12 flex-1 m-2.5 bg-[#DEDEDE]"/>
+                        <TextInput onBlur = {onBlur} onChangeText = {onChange} value = {value} className = "align-middle text-center w-11/12 flex-1 m-2.5 bg-[#DEDEDE]"/>
                     ); }}
                 />
-                <Text style = {{color: colours.black}} className="text-xl">Workout Notes</Text>
+                <Text style = {{ "color": colours.black }} className = "text-xl">Workout Notes</Text>
                 <Controller
                     control = {control}
                     name = "workoutNotes"
                     render = {({ "field": { onChange, onBlur, value } }) => { return (
-                        <TextInput onBlur = {onBlur} onChangeText = {onChange} value = {value} multiline = {true} numberOfLines = {3} className="align-middle text-center w-11/12 flex-1 m-2.5 bg-[#DEDEDE]"/>
+                        <TextInput onBlur = {onBlur} onChangeText = {onChange} value = {value} multiline = {true} numberOfLines = {3} className = "align-middle text-center w-11/12 flex-1 m-2.5 bg-[#DEDEDE]"/>
                     ); }}
                 /> 
-                <Text style = {{color: colours.black}} className="text-xl mt-[30px]">Exercises</Text>
-                <View className="items-center flex justify-center">
+                <Text style = {{ "color": colours.black }} className = "text-xl mt-[30px]">Exercises</Text>
+                <View className = "items-center flex justify-center">
                     {fields.map((field, index) => { return (
-                        <View key = {field.id} className="flex-initial flex-col w-full justify-between mt-[15px] flex-wrap items-center">
-                            <View className="flex-row w-full">
-                                <View className="bg-[#f0f0f0] items-center min-h-[100px] flex-1 m-2.5 p-{20px}">
-                                    <Text style = {{color: colours.black}} className="flex-1 text-[15px] h-5">Exercise Name</Text>
+                        <View key = {field.id} className = "flex-initial flex-col w-full justify-between mt-[15px] flex-wrap items-center">
+                            <View className = "flex-row w-full">
+                                <View className = "bg-[#f0f0f0] items-center min-h-[100px] flex-1 m-2.5 p-{20px}">
+                                    <Text style = {{ "color": colours.black }} className = "flex-1 text-[15px] h-5">Exercise Name</Text>
                                     <Controller
                                         control = {control}
                                         name = {`exercises.${index}.name`}
-                                        className="align-middle text-center w-11/12 flex-1 m-2.5 bg-[#DEDEDE] h-5"
+                                        className = "align-middle text-center w-11/12 flex-1 m-2.5 bg-[#DEDEDE] h-5"
                                         render = {({ "field": { onChange, onBlur, value } }) => { return (
                                             <TextInput
                                                 onBlur = {onBlur}
                                                 onChangeText = {onChange}
                                                 value = {value}
-                                                className="align-middle text-center w-11/12 flex-1 m-2.5 bg-[#DEDEDE]"
+                                                className = "align-middle text-center w-11/12 flex-1 m-2.5 bg-[#DEDEDE]"
                                             />
                                         ); }}
                                     />
                                 </View>
-                                <View className="bg-[#f0f0f0] items-center min-h-[100px] flex-1 m-2.5 p-{20px}">
-                                    <Text style = {{color: colours.black}} className="flex-1 text-[15px] h-5">Personal Best</Text>
-                                    <Text className="text-center w-11/12 flex-1 m-2.5 bg-[#DEDEDE] h-[27px] leading-[35px]">N/A</Text>
+                                <View className = "bg-[#f0f0f0] items-center min-h-[100px] flex-1 m-2.5 p-{20px}">
+                                    <Text style = {{ "color": colours.black }} className = "flex-1 text-[15px] h-5">Personal Best</Text>
+                                    <Text className = "text-center w-11/12 flex-1 m-2.5 bg-[#DEDEDE] h-[27px] leading-[35px]">N/A</Text>
                                 </View>
-                                <View className="bg-[#f0f0f0] items-center min-h-[100px] flex-1 m-2.5 p-{20px}">
-                                    <Text style = {{color: colours.black}} className="flex-1 text-[15px] h-5">Weight Size</Text>
+                                <View className = "bg-[#f0f0f0] items-center min-h-[100px] flex-1 m-2.5 p-{20px}">
+                                    <Text style = {{ "color": colours.black }} className = "flex-1 text-[15px] h-5">Weight Size</Text>
                                     <Controller
                                         control = {control}
                                         name = {`exercises.${index}.duration`}
-                                        className="align-middle text-center w-11/12 flex-1 m-2.5 bg-[#DEDEDE] h-5"
+                                        className = "align-middle text-center w-11/12 flex-1 m-2.5 bg-[#DEDEDE] h-5"
                                         render = {({ "field": { onChange, onBlur, value } }) => { return (
                                             <TextInput
                                                 onBlur = {onBlur}
                                                 onChangeText = {onChange}
                                                 value = {value}
                                                 keyboardType = "numeric"
-                                                className="align-middle text-center w-11/12 flex-1 m-2.5 bg-[#DEDEDE]"
+                                                className = "align-middle text-center w-11/12 flex-1 m-2.5 bg-[#DEDEDE]"
                                             />
                                         ); }}
                                     />
                                 </View>
-                                <View className="bg-[#f0f0f0] items-center min-h-[100px] flex-1 m-2.5 p-{20px}">
-                                    <Text style = {{color: colours.black}} className="flex-1 text-[15px] h-5">Reps</Text>
+                                <View className = "bg-[#f0f0f0] items-center min-h-[100px] flex-1 m-2.5 p-{20px}">
+                                    <Text style = {{ "color": colours.black }} className = "flex-1 text-[15px] h-5">Reps</Text>
                                     <Controller
                                         control = {control}
                                         name = {`exercises.${index}.reps`}
-                                        className="align-middle text-center w-11/12 flex-1 m-2.5 bg-[#DEDEDE] h-5"
+                                        className = "align-middle text-center w-11/12 flex-1 m-2.5 bg-[#DEDEDE] h-5"
                                         render = {({ "field": { onChange, onBlur, value } }) => { return (
                                             <TextInput
                                                 onBlur = {onBlur}
                                                 onChangeText = {onChange}
                                                 value = {value}
                                                 keyboardType = "numeric"
-                                                className="align-middle text-center w-11/12 flex-1 m-2.5 bg-[#DEDEDE]"
+                                                className = "align-middle text-center w-11/12 flex-1 m-2.5 bg-[#DEDEDE]"
                                             />
                                         ); }}
                                     />
@@ -131,8 +131,8 @@ const WorkoutForm = () => {
                                 <TouchableOpacity onPress = {() => {
                                     updateData();
                                     addSet(index);
-                                }} className="mt-[100px] bg-[#2296f3] p-2 m-[5px]">
-                                    <Text style = {{color: colours.white}} className="font-bold text-[16px]">Add Set</Text>
+                                }} className = "mt-[100px] bg-[#2296f3] p-2 m-[5px]">
+                                    <Text style = {{ "color": colours.white }} className = "font-bold text-[16px]">Add Set</Text>
                                 </TouchableOpacity>
                             )}
                         </View>
@@ -142,11 +142,11 @@ const WorkoutForm = () => {
                 <TouchableOpacity onPress = {() => {
                     updateData();
                     append({ "name": "", "duration": "", "reps": "" });
-                }} className="mt-[100px] bg-[#2296f3] p-2 m-[5px]">
-                    <Text style = {{color: colours.white}} className="font-bold text-[16px]">Add Exercise</Text>
+                }} className = "mt-[100px] bg-[#2296f3] p-2 m-[5px]">
+                    <Text style = {{ "color": colours.white }} className = "font-bold text-[16px]">Add Exercise</Text>
                 </TouchableOpacity>
-                <TouchableOpacity onPress = {handleSubmit(onSubmit)} className="mt-[100px] bg-[#2296f3] p-2 m-[5px]">
-                    <Text style={{color: colours.white}} className="font-bold text-[16px]">Submit</Text>
+                <TouchableOpacity onPress = {handleSubmit(onSubmit)} className = "mt-[100px] bg-[#2296f3] p-2 m-[5px]">
+                    <Text style = {{ "color": colours.white }} className = "font-bold text-[16px]">Submit</Text>
                 </TouchableOpacity>
             </View>
         </ScrollView>

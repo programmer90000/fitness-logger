2c8507950e0507377ac9614843748a058efc15fb
Abdul Rehman <abdulr568programming@outlook.com>
2024-10-05 17:12:09 +0100

    screen: Created Workout History screen

--- app/_layout.js
+++ app/_layout.js
@@ -9,6 +9,7 @@ export default function Layout() {
                 <Drawer.Screen name = "screens/create-a-new-workout-preset/create-a-new-workout-preset" options = {{ "drawerLabel": "Create A New Workout", "title": "Create A New Workout" }} />
                 <Drawer.Screen name = "screens/create-exercise/create-exercise" options = {{ "drawerLabel": "Create Exercise", "title": "Create Exercise" }} />
                 <Drawer.Screen name = "screens/create-workout/create-workout" options = {{ "drawerLabel": "Begin Workout", "title": "Begin Workout" }} />
+                <Drawer.Screen name = "screens/workout-history/workout-history" options = {{ "drawerLabel": "Workout History", "title": "Workout History" }} />
             </Drawer>
         </GestureHandlerRootView>
     );

014063218c67b9128fc3768a15027bd280f7e13e
Abdul Rehman <127865680+programmer90000@users.noreply.github.com>
2024-10-12 10:01:05 +0100

    screen: Created Set A Goal screen (#47)

---
+++ app/screens/create-a-new-workout-preset/create-a-new-workout-preset.js
@@ -0,0 +1,10 @@
+import React from "react";
+import WorkoutForm from "../../components/workout-form/workout-form.js";
+
+const createANewWorkoutPreset = () => {
+    return (
+        <WorkoutForm />
+    );
+};
+
+export default createANewWorkoutPreset;

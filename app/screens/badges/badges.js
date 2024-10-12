58335a877bfb5263e0fdea63f86ed0ce61dc1fb1
Abdul Rehman <abdulr568programming@outlook.com>
2024-10-12 17:49:15 +0100

    style: Styling for completed and uncompleted badges

--- app/screens/badges/badges.js
+++ app/screens/badges/badges.js
@@ -8,9 +8,15 @@ const styles = StyleSheet.create({
         "margin": 20,
     },
     "text": {
-        "fontSize": 16,
+        "fontSize": 32,
         "fontWeight": "bold",
         "textAlign": "center",
+    },
+    "completed": {
+        "color": "#FFD700",
+    },
+    "unCompleted": {
+        "color": "#000000",
     },
 });
 
@@ -18,40 +24,40 @@ export default function Footer() {
     return (
         <ScrollView contentContainerStyle = {{ "flexDirection": "row", "alignItems": "center", "justifyContent": "center", "flexWrap": "wrap" }}>
             <View style = {styles.badge}>
-                <Text style = {styles.text}>Badge 1</Text>
-                <MaterialCommunityIcons name = "shoe-cleat" size = {100} color = "black" />
+                <Text style = {[styles.text, styles.completed]}>Badge 1</Text>
+                <MaterialCommunityIcons name = "shoe-cleat" size = {100} style = {styles.completed} />
             </View>
             <View style = {styles.badge}>
-                <Text style = {styles.text}>Badge 2</Text>
-                <FontAwesome6 name = "dumbbell" size = {100} color = "black" />
+                <Text style = {[styles.text, styles.completed]}>Badge 2</Text>
+                <FontAwesome6 name = "dumbbell" size = {100} style = {styles.completed} />
             </View>
             <View style = {styles.badge}>
-                <Text style = {styles.text}>Badge 3</Text>
-                <FontAwesome6 name = "mountain" size = {100} color = "black" />
+                <Text style = {[styles.text, styles.completed]}>Badge 3</Text>
+                <FontAwesome6 name = "mountain" size = {100} style = {styles.completed} />
             </View>
             <View style = {styles.badge}>
-                <Text style = {styles.text}>Badge 4</Text>
-                <Entypo name = "star" size = {100} color = "black" />
+                <Text style = {[styles.text, styles.completed]}>Badge 4</Text>
+                <Entypo name = "star" size = {100} style = {styles.completed} />
             </View>
             <View style = {styles.badge}>
-                <Text style = {styles.text}>Badge 5</Text>
-                <FontAwesome6 name = "medal" size = {100} color = "black" />
+                <Text style = {[styles.text, styles.completed]}>Badge 5</Text>
+                <FontAwesome6 name = "medal" size = {100} style = {styles.completed} />
             </View>
             <View style = {styles.badge}>
-                <Text style = {styles.text}>Badge 6</Text>
-                <Entypo name = "heart" size = {100} color = "black" />
+                <Text style = {[styles.text, styles.unCompleted]}>Badge 6</Text>
+                <Entypo name = "heart" size = {100} style = {styles.unCompleted} />
             </View>
             <View style = {styles.badge}>
-                <Text style = {styles.text}>Badge 7</Text>
-                <FontAwesome6 name = "fire" size = {100} color = "black" />
+                <Text style = {[styles.text, styles.unCompleted]}>Badge 7</Text>
+                <FontAwesome6 name = "fire" size = {100} style = {styles.unCompleted} />
             </View>
             <View style = {styles.badge}>
-                <Text style = {styles.text}>Badge 8</Text>
-                <FontAwesome6 name = "trophy" size = {100} color = "black" />
+                <Text style = {[styles.text, styles.unCompleted]}>Badge 8</Text>
+                <FontAwesome6 name = "trophy" size = {100} style = {styles.unCompleted} />
             </View>
             <View style = {styles.badge}>
-                <Text style = {styles.text}>Badge 9</Text>
-                <FontAwesome6 name = "bottle-water" size = {100} color = "black" />
+                <Text style = {[styles.text, styles.unCompleted]}>Badge 9</Text>
+                <FontAwesome6 name = "bottle-water" size = {100} style = {styles.unCompleted} />
             </View>
         </ScrollView>
     );

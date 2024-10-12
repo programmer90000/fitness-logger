d57511d7ca7941fad72421a83bd1ec0c10abaf62
Abdul Rehman <abdulr568programming@outlook.com>
2024-10-12 17:41:36 +0100

    screen: Added text

--- app/screens/badges/badges.js
+++ app/screens/badges/badges.js
@@ -1,26 +1,58 @@
-import { ScrollView, StyleSheet } from "react-native";
+import { ScrollView, View, Text, StyleSheet } from "react-native";
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
 import Entypo from "@expo/vector-icons/Entypo";
 
 const styles = StyleSheet.create({
-    "container": {
-        "padding": 16,
+    "badge": {
+        "margin": 20,
+    },
+    "text": {
+        "fontSize": 16,
+        "fontWeight": "bold",
+        "textAlign": "center",
     },
 });
 
 export default function Footer() {
     return (
         <ScrollView contentContainerStyle = {{ "flexDirection": "row", "alignItems": "center", "justifyContent": "center", "flexWrap": "wrap" }}>
-            <MaterialCommunityIcons name = "shoe-cleat" size = {100} color = "black" />
-            <FontAwesome6 name = "dumbbell" size = {100} color = "black" />
-            <FontAwesome6 name = "mountain" size = {100} color = "black" />
-            <Entypo name = "star" size = {100} color = "black" />
-            <FontAwesome6 name = "medal" size = {100} color = "black" />
-            <Entypo name = "heart" size = {100} color = "black" />
-            <FontAwesome6 name = "fire" size = {100} color = "black" />
-            <FontAwesome6 name = "trophy" size = {100} color = "black" />
-            <FontAwesome6 name = "bottle-water" size = {100} color = "black" />
+            <View style = {styles.badge}>
+                <Text style = {styles.text}>Badge 1</Text>
+                <MaterialCommunityIcons name = "shoe-cleat" size = {100} color = "black" />
+            </View>
+            <View style = {styles.badge}>
+                <Text style = {styles.text}>Badge 2</Text>
+                <FontAwesome6 name = "dumbbell" size = {100} color = "black" />
+            </View>
+            <View style = {styles.badge}>
+                <Text style = {styles.text}>Badge 3</Text>
+                <FontAwesome6 name = "mountain" size = {100} color = "black" />
+            </View>
+            <View style = {styles.badge}>
+                <Text style = {styles.text}>Badge 4</Text>
+                <Entypo name = "star" size = {100} color = "black" />
+            </View>
+            <View style = {styles.badge}>
+                <Text style = {styles.text}>Badge 5</Text>
+                <FontAwesome6 name = "medal" size = {100} color = "black" />
+            </View>
+            <View style = {styles.badge}>
+                <Text style = {styles.text}>Badge 6</Text>
+                <Entypo name = "heart" size = {100} color = "black" />
+            </View>
+            <View style = {styles.badge}>
+                <Text style = {styles.text}>Badge 7</Text>
+                <FontAwesome6 name = "fire" size = {100} color = "black" />
+            </View>
+            <View style = {styles.badge}>
+                <Text style = {styles.text}>Badge 8</Text>
+                <FontAwesome6 name = "trophy" size = {100} color = "black" />
+            </View>
+            <View style = {styles.badge}>
+                <Text style = {styles.text}>Badge 9</Text>
+                <FontAwesome6 name = "bottle-water" size = {100} color = "black" />
+            </View>
         </ScrollView>
     );
 }

b8dabeb732c229cd2004de6f4bd3dda4c21c7a8e
Abdul Rehman <abdulr568programming@outlook.com>
2024-10-12 17:06:31 +0100

    screen: Updated size of icons

--- app/screens/badges/badges.js
+++ app/screens/badges/badges.js
@@ -6,15 +6,15 @@ import Entypo from "@expo/vector-icons/Entypo";
 export default function Footer() {
     return (
         <View>
-            <MaterialCommunityIcons name = "shoe-cleat" size = {24} color = "black" />
-            <FontAwesome6 name = "dumbbell" size = {24} color = "black" />
-            <FontAwesome6 name = "mountain" size = {24} color = "black" />
-            <Entypo name = "star" size = {24} color = "black" />
-            <FontAwesome6 name = "medal" size = {24} color = "black" />
-            <Entypo name = "heart" size = {24} color = "black" />
-            <FontAwesome6 name = "fire" size = {24} color = "black" />
-            <FontAwesome6 name = "trophy" size = {24} color = "black" />
-            <FontAwesome6 name = "bottle-water" size = {24} color = "black" />
+            <MaterialCommunityIcons name = "shoe-cleat" size = {100} color = "black" />
+            <FontAwesome6 name = "dumbbell" size = {100} color = "black" />
+            <FontAwesome6 name = "mountain" size = {100} color = "black" />
+            <Entypo name = "star" size = {100} color = "black" />
+            <FontAwesome6 name = "medal" size = {100} color = "black" />
+            <Entypo name = "heart" size = {100} color = "black" />
+            <FontAwesome6 name = "fire" size = {100} color = "black" />
+            <FontAwesome6 name = "trophy" size = {100} color = "black" />
+            <FontAwesome6 name = "bottle-water" size = {100} color = "black" />
         </View>
     );
 }

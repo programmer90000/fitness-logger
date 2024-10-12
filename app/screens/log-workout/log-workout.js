369c32673f51d854210a7f5814df174d011533ce
Abdul Rehman <127865680+programmer90000@users.noreply.github.com>
2024-10-12 10:00:54 +0100

    screen: Created Set A Goal screen (#45)

---
+++ app/screens/log-workout/log-workout.js
@@ -0,0 +1,10 @@
+import React from "react";
+import WorkoutForm from "../../components/workout-form/workout-form.js";
+
+const logWorkout = () => {
+    return (
+        <WorkoutForm />
+    );
+};
+
+export default logWorkout;

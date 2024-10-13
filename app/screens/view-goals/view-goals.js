375d489ccd52637ced5a9bc420513facbac0bf1f
Abdul Rehman <127865680+programmer90000@users.noreply.github.com>
2024-10-13 11:02:59 +0100

    screen: Created View Goals screen (#121)

---
+++ app/screens/view-goals/view-goals.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { ScrollView, Text, TouchableOpacity } from "react-native";
+
+const colours = {
+    "black": "#060606",
+    "white": "#f1f1f1",
+    "red": "#d10000",
+};
+
+const goals = () => {
+    return (
+        <ScrollView className = "mt-5">
+            <TouchableOpacity style = {{ "backgroundColor": "#FF0000" }} className = "p-2 h-20 justify-center mt-[5px] w-4/6 items-center self-center mb-5">
+                <Text className = "font-medium text-base" style = {{ "color": colours.black }}>Goal 1</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style = {{ "backgroundColor": "#FF0000" }} className = "p-2 h-20 justify-center mt-[5px] w-4/6 items-center self-center mb-5">
+                <Text className = "font-medium text-base" style = {{ "color": colours.black }}>Goal 2</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style = {{ "backgroundColor": "#FF0000" }} className = "p-2 h-20 justify-center mt-[5px] w-4/6 items-center self-center mb-5">
+                <Text className = "font-medium text-base" style = {{ "color": colours.black }}>Goal 3</Text>
+            </TouchableOpacity>
+        </ScrollView>
+    );
+};
+
+export default goals;

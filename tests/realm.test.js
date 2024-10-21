a0ef7e685129f2846796f64061851d635b7aa2f6
Abdul Rehman <abdulr568programming@outlook.com>
2024-10-21 19:50:45 +0100

    test: Test descriptions for workoutPresets database

---
+++ tests/realm.test.js
@@ -0,0 +1,9 @@
+import { React } from "react";
+
+test("Create record in workoutPresets database", () => { });
+
+test("Read record in workoutPresets database", () => { });
+
+test("Update record in workoutPresets database", () => { });
+
+test("Delete record in workoutPresets database", () => { });

4bef85a459cb34fa4844e6e7ccfd5cfff5edc85f
Abdul Rehman <abdulr568programming@outlook.com>
2024-10-21 19:58:00 +0100

    test: Added comment above each group of tests

--- tests/realm.test.js
+++ tests/realm.test.js
@@ -1,5 +1,6 @@
 import { React } from "react";
 
+// ! workoutPresets database
 test("Create record in workoutPresets database", () => { });
 
 test("Read record in workoutPresets database", () => { });
@@ -8,6 +9,7 @@ test("Update record in workoutPresets database", () => { });
 
 test("Delete record in workoutPresets database", () => { });
 
+// ! exercises database
 test("Create record in exercises database", () => { });
 
 test("Read record in exercises database", () => { });
@@ -16,6 +18,7 @@ test("Update record in exercises database", () => { });
 
 test("Delete record in exercises database", () => { });
 
+// ! workoutPresetsExercises database
 test("Create record in workoutPresetsExercises database", () => { });
 
 test("Read record in workoutPresetsExercises database", () => { });
@@ -24,6 +27,7 @@ test("Update record in workoutPresetsExercises database", () => { });
 
 test("Delete record in workoutPresetsExercises database", () => { });
 
+// ! workoutPresetsExercises database
 test("Create record in previousWorkouts database", () => { });
 
 test("Read record in previousWorkouts database", () => { });
@@ -32,6 +36,7 @@ test("Update record in previousWorkouts database", () => { });
 
 test("Delete record in previousWorkouts database", () => { });
 
+// ! workoutPresetsExercises database
 test("Create record in previousWorkoutsExercises database", () => { });
 
 test("Read record in previousWorkoutsExercises database", () => { });
@@ -40,6 +45,7 @@ test("Update record in previousWorkoutsExercises database", () => { });
 
 test("Delete record in previousWorkoutsExercises database", () => { });
 
+// ! workoutPresetsExercises database
 test("Create record in badges database", () => { });
 
 test("Read record in badges database", () => { });
@@ -48,6 +54,7 @@ test("Update record in badges database", () => { });
 
 test("Delete record in badges database", () => { });
 
+// ! workoutPresetsExercises database
 test("Create record in goals database", () => { });
 
 test("Read record in goals database", () => { });

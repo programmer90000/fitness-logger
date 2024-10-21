1cc62b58128b6f91f5d8904b21726d1c7e9ea255
Abdul Rehman <abdulr568programming@outlook.com>
2024-10-21 19:51:25 +0100

    test: Test descriptions for exercises database

--- tests/realm.test.js
+++ tests/realm.test.js
@@ -7,3 +7,11 @@ test("Read record in workoutPresets database", () => { });
 test("Update record in workoutPresets database", () => { });
 
 test("Delete record in workoutPresets database", () => { });
+
+test("Create record in exercises database", () => { });
+
+test("Read record in exercises database", () => { });
+
+test("Update record in exercises database", () => { });
+
+test("Delete record in exercises database", () => { });

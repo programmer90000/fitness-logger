8ad66cbe753a13ccecb4188d739566bd92f2a620
Abdul Rehman <abdulr568programming@outlook.com>
2024-10-21 19:10:44 +0100

    test: Snapshot test for badges.js

--- tests/snapshots.test.js
+++ tests/snapshots.test.js
@@ -1,9 +1,16 @@
 import { React } from "react";
 import renderer from "react-test-renderer";
 import App from "../app/index";
+import Badges from "../app/screens/badges/badges";
 
 test("index.js Test", () => {
     const snapshot = renderer.create(<App />);
+    let snapshotJSON = snapshot.toJSON();
+    expect(snapshotJSON).toMatchSnapshot();
+});
+
+test("badges.js Test", () => {
+    const snapshot = renderer.create(<Badges />);
     let snapshotJSON = snapshot.toJSON();
     expect(snapshotJSON).toMatchSnapshot();
 });

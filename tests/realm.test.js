a1624d46de116314799e3cb5dd76146523582c55
Abdul Rehman <127865680+programmer90000@users.noreply.github.com>
2024-10-21 20:03:51 +0100

    test: Test descriptions for database (#159)

---
+++ tests/realm.test.js
@@ -0,0 +1,64 @@
+import { React } from "react";
+
+// ! workoutPresets database
+test("Create record in workoutPresets database", () => { });
+
+test("Read record in workoutPresets database", () => { });
+
+test("Update record in workoutPresets database", () => { });
+
+test("Delete record in workoutPresets database", () => { });
+
+// ! exercises database
+test("Create record in exercises database", () => { });
+
+test("Read record in exercises database", () => { });
+
+test("Update record in exercises database", () => { });
+
+test("Delete record in exercises database", () => { });
+
+// ! workoutPresetsExercises database
+test("Create record in workoutPresetsExercises database", () => { });
+
+test("Read record in workoutPresetsExercises database", () => { });
+
+test("Update record in workoutPresetsExercises database", () => { });
+
+test("Delete record in workoutPresetsExercises database", () => { });
+
+// ! workoutPresetsExercises database
+test("Create record in previousWorkouts database", () => { });
+
+test("Read record in previousWorkouts database", () => { });
+
+test("Update record in previousWorkouts database", () => { });
+
+test("Delete record in previousWorkouts database", () => { });
+
+// ! workoutPresetsExercises database
+test("Create record in previousWorkoutsExercises database", () => { });
+
+test("Read record in previousWorkoutsExercises database", () => { });
+
+test("Update record in previousWorkoutsExercises database", () => { });
+
+test("Delete record in previousWorkoutsExercises database", () => { });
+
+// ! workoutPresetsExercises database
+test("Create record in badges database", () => { });
+
+test("Read record in badges database", () => { });
+
+test("Update record in badges database", () => { });
+
+test("Delete record in badges database", () => { });
+
+// ! workoutPresetsExercises database
+test("Create record in goals database", () => { });
+
+test("Read record in goals database", () => { });
+
+test("Update record in goals database", () => { });
+
+test("Delete record in goals database", () => { });

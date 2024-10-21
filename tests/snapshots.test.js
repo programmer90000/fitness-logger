76b9c7993152d0010eb43f9e650a3f61eed9a6b4
Abdul Rehman <abdulr568programming@outlook.com>
2024-10-21 19:13:17 +0100

    test: Snapshot test for create-a-new-workout-preset.js

--- tests/snapshots.test.js
+++ tests/snapshots.test.js
@@ -2,6 +2,7 @@ import { React } from "react";
 import renderer from "react-test-renderer";
 import App from "../app/index";
 import Badges from "../app/screens/badges/badges";
+import CreateANewWorkoutPreset from "../app/screens/create-a-new-workout-preset/create-a-new-workout-preset";
 
 test("index.js Test", () => {
     const snapshot = renderer.create(<App />);
@@ -11,6 +12,12 @@ test("index.js Test", () => {
 
 test("badges.js Test", () => {
     const snapshot = renderer.create(<Badges />);
+    let snapshotJSON = snapshot.toJSON();
+    expect(snapshotJSON).toMatchSnapshot();
+});
+
+test("create-a-new-workout-preset.js Test", () => {
+    const snapshot = renderer.create(<CreateANewWorkoutPreset />);
     let snapshotJSON = snapshot.toJSON();
     expect(snapshotJSON).toMatchSnapshot();
 });

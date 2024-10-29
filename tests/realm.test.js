06a0749ac554435aac7bfbf0e1f00251884ab7e6
Abdul Rehman <abdulr568programming@outlook.com>
2024-10-29 12:41:56 +0000

    test: Added description to each set of tests

--- tests/realm.test.js
+++ tests/realm.test.js
@@ -1,78 +1,99 @@
 import { React } from "react";
 
 // ! workoutPresets table
-test("Create workoutPresets table", () => { });
+describe("workoutPresets table", () => {
+    
+    test("Create workoutPresets table", () => { });
 
-test("Create record in workoutPresets table", () => { });
+    test("Create record in workoutPresets table", () => { });
 
-test("Read record in workoutPresets table", () => { });
+    test("Read record in workoutPresets table", () => { });
 
-test("Update record in workoutPresets table", () => { });
+    test("Update record in workoutPresets table", () => { });
 
-test("Delete record in workoutPresets table", () => { });
+    test("Delete record in workoutPresets table", () => { });
+});
 
 // ! exercises table
-test("Create exercises table", () => { });
+describe("exercises table", () => {
+    
+    test("Create exercises table", () => { });
 
-test("Create record in exercises table", () => { });
+    test("Create record in exercises table", () => { });
 
-test("Read record in exercises table", () => { });
+    test("Read record in exercises table", () => { });
 
-test("Update record in exercises table", () => { });
+    test("Update record in exercises table", () => { });
 
-test("Delete record in exercises table", () => { });
+    test("Delete record in exercises table", () => { });
+});
 
 // ! workoutPresetsExercises table
-test("Create workoutPresetsExercises table", () => { });
+describe("workoutPresetsExercises table", () => {
 
-test("Create record in workoutPresetsExercises table", () => { });
+    test("Create workoutPresetsExercises table", () => { });
 
-test("Read record in workoutPresetsExercises table", () => { });
+    test("Create record in workoutPresetsExercises table", () => { });
 
-test("Update record in workoutPresetsExercises table", () => { });
+    test("Read record in workoutPresetsExercises table", () => { });
 
-test("Delete record in workoutPresetsExercises table", () => { });
+    test("Update record in workoutPresetsExercises table", () => { });
+
+    test("Delete record in workoutPresetsExercises table", () => { });
+});
 
 // ! previousWorkouts table
-test("Create previousWorkouts table", () => { });
+describe("previousWorkouts table", () => {
 
-test("Create record in previousWorkouts table", () => { });
+    test("Create previousWorkouts table", () => { });
 
-test("Read record in previousWorkouts table", () => { });
+    test("Create record in previousWorkouts table", () => { });
 
-test("Update record in previousWorkouts table", () => { });
+    test("Read record in previousWorkouts table", () => { });
 
-test("Delete record in previousWorkouts table", () => { });
+    test("Update record in previousWorkouts table", () => { });
+
+    test("Delete record in previousWorkouts table", () => { });
+});
 
 // ! previousWorkoutsExercises table
-test("Create previousWorkoutsExercises table", () => { });
+describe("previousWorkoutsExercises table", () => {
 
-test("Create record in previousWorkoutsExercises table", () => { });
+    test("Create previousWorkoutsExercises table", () => { });
 
-test("Read record in previousWorkoutsExercises table", () => { });
+    test("Create record in previousWorkoutsExercises table", () => { });
 
-test("Update record in previousWorkoutsExercises table", () => { });
+    test("Read record in previousWorkoutsExercises table", () => { });
 
-test("Delete record in previousWorkoutsExercises table", () => { });
+    test("Update record in previousWorkoutsExercises table", () => { });
+
+    test("Delete record in previousWorkoutsExercises table", () => { });
+});
 
 // ! badges table
-test("Create badges table", () => { });
+describe("badges table", () => {
 
-test("Create record in badges table", () => { });
+    test("Create badges table", () => { });
 
-test("Read record in badges table", () => { });
+    test("Create record in badges table", () => { });
 
-test("Update record in badges table", () => { });
+    test("Read record in badges table", () => { });
 
-test("Delete record in badges table", () => { });
+    test("Update record in badges table", () => { });
+
+    test("Delete record in badges table", () => { });
+});
 
 // ! goals table
-test("Create goals table", () => { });
+describe("goals table", () => {
 
-test("Create record in goals table", () => { });
+    test("Create goals table", () => { });
 
-test("Read record in goals table", () => { });
+    test("Create record in goals table", () => { });
 
-test("Update record in goals table", () => { });
+    test("Read record in goals table", () => { });
 
-test("Delete record in goals table", () => { });
+    test("Update record in goals table", () => { });
+
+    test("Delete record in goals table", () => { });
+});

c824aa17533ec70e7779a009516d0f7ff1cfc73b
Abdul Rehman <abdulr568programming@outlook.com>
2024-11-30 18:32:29 +0000

    screen: Created Create Badge screen

---
+++ app/screens/create-badge/create-badge.js
@@ -0,0 +1,113 @@
+import React, { useState } from "react";
+import { View, ScrollView, Text, TextInput, TouchableOpacity } from "react-native";
+import { useForm, Controller, useFieldArray } from "react-hook-form";
+import DropdownComponent from "../../components/dropdown-box/dropdown-box";
+import { exercises, badges } from "../../../database/realm-database.js";
+import Realm from "realm";
+import UploadMedia from "../../components/upload-media/upload-media.js";
+
+const colours = {
+    "black": "#060606",
+    "white": "#f1f1f1",
+    "red": "#d10000",
+};
+
+const WorkoutForm = () => {
+    const [goalName, setGoalName] = useState(null);
+    const [selectedExerciseType, setSelectedExerciseType] = useState(null);
+    const { control, handleSubmit, getValues, setValue } = useForm({});
+    const [videoPath, setVideoPath] = useState(null);
+    const [name, setName] = useState(null);
+    
+    const { fields, append, insert, remove } = useFieldArray({ control, "name": "exercises" });
+
+    const realm = new Realm({ "schema": [exercises] });
+    const allExercises = realm.objects("Exercises");
+    const names = allExercises.map((exercise) => {
+        return {
+            "label": exercise.name,
+            "value": exercise.name.replace(/\s/g, ""), // Remove spaces using regex
+            "type": exercise.type,
+        };
+    });
+    realm.close();
+
+    const onSubmit = () => {
+        const formValues = getValues();
+        const realm = new Realm({ "schema": [badges] });
+        realm.write(() => {
+            const currentHighestId = realm.objects("Badges").max("id") || 0;
+            let newId;
+
+            if (currentHighestId === 0)
+            {
+                newId = 1;
+            } else {
+                newId = currentHighestId + 1;
+            }
+            
+            realm.create("Badges", { "id": newId, "image": formValues.goalName, "text": formValues.goalNotes, "completed": false });
+        });
+        const allBadges = realm.objects("Badges");
+        console.log("All Badges:", allBadges);
+        realm.close();
+    };
+
+    return (
+        <ScrollView style = {{ "backgroundColor": colours.white }}>
+            <View className = "items-center m-[5px]">
+                <Text style = {{ "color": colours.black }} className = "text-xl">Goal Name</Text>
+                <Controller control = {control} name = "goalName" render = {({ "field": { onChange, onBlur, value } }) => { return (<TextInput onBlur = {onBlur} onChangeText = {(text) => {
+                    onChange(text); 
+                    setGoalName(text);
+                }} value = {value} className = "align-middle text-center w-11/12 flex-1 m-2.5 bg-[#DEDEDE]"/>
+                ); }} />
+                <DropdownComponent data = {names} value = {name} onChange = {(name) => {
+                    setName(name);
+                    setValue("exercises.name", name);
+                    setSelectedExerciseType(names.find((item) => { return item.value === name; })?.type);                 
+                }} style = {{ "width": 100 }} placeholder = "Exercise Name" />
+                
+                {selectedExerciseType === "reps" && (
+                    <View className = "bg-[#f0f0f0] items-center min-h-[100px] flex-1 m-2.5 p-{20px}">
+                        <Text>Number of reps</Text>
+                        <Controller control = {control} name = {"reps"} className = "align-middle text-center w-11/12 flex-1 m-2.5 bg-[#DEDEDE] h-5" render = {({ "field": { onChange, onBlur, value } }) => { return (<TextInput onBlur = {onBlur} onChangeText = {onChange} value = {value} keyboardType = "numeric" className = "align-middle text-center w-11/12 flex-1 m-2.5 bg-[#DEDEDE]" />
+                        ); }} />
+                    </View>
+                )}
+                {selectedExerciseType === "weightAndReps" && (
+                    <View className = "bg-[#f0f0f0] items-center min-h-[100px] flex-1 m-2.5 p-{20px}">
+                        <Text>Amount of Weight</Text>
+                        <Controller control = {control} name = {"reps"} className = "align-middle text-center w-11/12 flex-1 m-2.5 bg-[#DEDEDE] h-5" render = {({ "field": { onChange, onBlur, value } }) => {
+                            return (
+                                <TextInput onBlur = {onBlur} onChangeText = {onChange} value = {value} keyboardType = "numeric" className = "align-middle text-center w-11/12 flex-1 m-2.5 bg-[#DEDEDE]" />
+                            ); }} />
+                        <Text>Number of Reps</Text>
+                        <Controller control = {control} name = {"reps"} className = "align-middle text-center w-11/12 flex-1 m-2.5 bg-[#DEDEDE] h-5" render = {({ "field": { onChange, onBlur, value } }) => {
+                            return (
+                                <TextInput onBlur = {onBlur} onChangeText = {onChange} value = {value} keyboardType = "numeric" className = "align-middle text-center w-11/12 flex-1 m-2.5 bg-[#DEDEDE]" />
+                            ); }} />
+                    </View>
+                )}
+                {selectedExerciseType === "distanceAndTime" && (
+                    <View className = "bg-[#f0f0f0] items-center min-h-[100px] flex-1 m-2.5 p-{20px}">
+                        <Text>Distance</Text>
+                        <Controller control = {control} name = {"reps"} className = "align-middle text-center w-11/12 flex-1 m-2.5 bg-[#DEDEDE] h-5" render = {({ "field": { onChange, onBlur, value } }) => { return (<TextInput onBlur = {onBlur} onChangeText = {onChange} value = {value} keyboardType = "numeric" className = "align-middle text-center w-11/12 flex-1 m-2.5 bg-[#DEDEDE]" />
+                        ); }} />
+                        <Text>Time</Text>
+                        <Controller control = {control} name = {"reps"} className = "align-middle text-center w-11/12 flex-1 m-2.5 bg-[#DEDEDE] h-5" render = {({ "field": { onChange, onBlur, value } }) => { return (
+                            <TextInput onBlur = {onBlur} onChangeText = {onChange} value = {value} keyboardType = "numeric" className = "align-middle text-center w-11/12 flex-1 m-2.5 bg-[#DEDEDE]" />
+                        ); }} />
+                    </View>
+                )}
+
+                <UploadMedia onMediaSelect = {(path) => { return setVideoPath(path); }} mediaFileName = {`${goalName}.mp4`} mediaType = "Image" />
+                <TouchableOpacity onPress = {handleSubmit(onSubmit)} className = "mt-[100px] bg-[#2296f3] p-2 m-[5px]">
+                    <Text style = {{ "color": colours.white }} className = "font-bold text-[16px]">Create Goal</Text>
+                </TouchableOpacity>
+            </View>
+        </ScrollView>
+    );
+};
+
+export default WorkoutForm;

3edcb698d47a57947c7aa162f7e4b7a4a5027a26
Abdul Rehman <abdulr568programming@outlook.com>
2024-12-03 17:21:38 +0000

    style: Reordered colours

--- app/constants/colours.js
+++ app/constants/colours.js
@@ -1,19 +1,19 @@
 const colours = {
-    "red": "#FF0000",
+    "white": "#FFFFFF",
+    "off_white": "#f1f1f1",
+    "black": "#000000",
+    "charcoal": "#060606",
     "light_grey": "#DEDEDE",
     "very_light_grey": "#F0F0F0",
-    "charcoal": "#060606",
-    "off_white": "#f1f1f1",
-    "white": "#FFFFFF",
-    "black": "#000000",
+    "red": "#FF0000",
     "crimson": "#d10000",
     "carrot_orange": "#E26A00",
     "light_carrot_orange": "#FB8C00",
     "vivid_orange": "#FFA726",
-    "gold": "#FFD700",
     "light_blue": "#2296F3",
     "alice_blue": "#F1F8FF",
     "aqua_haze": "#F6F8FA",
+    "gold": "#FFD700",
 };
 
 export { colours };

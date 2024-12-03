1ea5165c2d926efd93962f8496fe2e244864bc04
Abdul Rehman <abdulr568programming@outlook.com>
2024-12-03 16:05:06 +0000

    style: Renamed colour keys

--- app/constants/colours.js
+++ app/constants/colours.js
@@ -1,7 +1,7 @@
 const colours = {
-    "black": "#060606",
-    "white": "#f1f1f1",
-    "red": "#d10000",
+    "charcoal": "#060606",
+    "off_white": "#f1f1f1",
+    "crimson": "#d10000",
 };
 
 export { colours };

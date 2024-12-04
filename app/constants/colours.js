a29f8616dc03228fb210083d08ac1de2782bee33
Abdul Rehman <abdulr568programming@outlook.com>
2024-12-04 14:42:52 +0000

    style: Added colours for dark mode

--- app/constants/colours.js
+++ app/constants/colours.js
@@ -1,4 +1,4 @@
-const colours = {
+const light_mode = {
     "colour_1": "#FFFFFF", // White
     "colour_2": "#F1F1F1", // Very Light Grey
     "colour_3": "#000000", // Black
@@ -16,4 +16,22 @@ const colours = {
     "colour_15": "#FFD700", // Yellow
 };
 
-export { colours };
+const dark_mode = {
+    "colour_1": "#000000",
+    "colour_2": "#0A0A0A",
+    "colour_3": "#FFFFFF",
+    "colour_4": "#F0F0F0",
+    "colour_5": "#7F7F7F",
+    "colour_6": "#ACACAC",
+    "colour_7": "#B71C1C",
+    "colour_8": "#B71C1C",
+    "colour_9": "#121212",
+    "colour_10": "#1D1D1D",
+    "colour_11": "#333333",
+    "colour_12": "#005EA8",
+    "colour_13": "#F1F8FF",
+    "colour_14": "#F6F8FA",
+    "colour_15": "#FFAB00",           
+};
+
+export { light_mode as colours };

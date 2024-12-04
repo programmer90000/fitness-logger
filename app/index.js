54711e64415003ca827a23ab266e336dc06248a1
Abdul Rehman <abdulr568programming@outlook.com>
2024-12-04 18:51:53 +0000

    feat: Initialize default storage values on first launch

--- app/index.js
+++ app/index.js
@@ -1,10 +1,29 @@
+import React, { useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { styles } from "./style.js";
-
 import Footer from "./components/Footer/Footer.js";
 
-export default function App() {
+const initializeStorage = async () => {
+    const defaultValues = {
+        "theme": "light",
+        "weight": "metric",
+        "distance": "kilometers",
+    };
+        
+    try {
+        for (const [key, value] of Object.entries(defaultValues)) {
+            const existingKey = await AsyncStorage.getItem(key);
+            if (existingKey === null) { await AsyncStorage.setItem(key, JSON.stringify(value)); }
+        }
+    } catch (error) {
+        console.error("Error initializing default values:", error);
+    }
+};
+
+const App = () => {
+    useEffect(() => { initializeStorage(); }, []);
     return (
         <View style = {styles.container}>
             <Text>Open up App.js to start working on your app!</Text>
@@ -12,4 +31,6 @@ export default function App() {
             <Footer />
         </View>
     );
-}
+};
+
+export default App;

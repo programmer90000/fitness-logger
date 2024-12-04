7cd7410a86460202d07c26901b65fbcedf3720a7
Abdul Rehman <abdulr568programming@outlook.com>
2024-12-04 12:01:57 +0000

    style: Added comments stating what each colour is

--- app/constants/colours.js
+++ app/constants/colours.js
@@ -1,19 +1,19 @@
 const colours = {
-    "colour_1": "#FFFFFF",
-    "colour_2": "#F1F1F1",
-    "colour_3": "#000000",
-    "colour_4": "#060606",
-    "colour_5": "#DEDEDE",
-    "colour_6": "#F0F0F0",
-    "colour_7": "#FF0000",
-    "colour_8": "#D10000",
-    "colour_9": "#E26A00",
-    "colour_10": "#FB8C00",
-    "colour_11": "#FFA726",
-    "colour_12": "#2296F3",
-    "colour_13": "#F1F8FF",
-    "colour_14": "#F6F8FA",
-    "colour_15": "#FFD700",
+    "colour_1": "#FFFFFF", // White
+    "colour_2": "#F1F1F1", // Very Light Grey
+    "colour_3": "#000000", // Black
+    "colour_4": "#060606", // Dark Grey
+    "colour_5": "#DEDEDE", // Light Grey
+    "colour_6": "#F0F0F0", // Very Light Grey
+    "colour_7": "#FF0000", // Red
+    "colour_8": "#D10000", // Strong Red
+    "colour_9": "#E26A00", // Dark Orange
+    "colour_10": "#FB8C00", // Orange
+    "colour_11": "#FFA726", // Vivid Orange
+    "colour_12": "#2296F3", // Vivid Blue
+    "colour_13": "#F1F8FF", // Very Light Blue
+    "colour_14": "#F6F8FA", // Grayish Blue
+    "colour_15": "#FFD700", // Yellow
 };
 
 export { colours };

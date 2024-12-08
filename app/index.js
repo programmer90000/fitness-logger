8cb89d7ae80dc35da55ecfc6efeacc9c65ec4598
Abdul Rehman <abdulr568programming@outlook.com>
2024-12-08 17:24:39 +0000

    feat: Console log theme in App.js

--- app/index.js
+++ app/index.js
@@ -1,10 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 import { styles } from "./style.js";
-
 import Footer from "./components/Footer/Footer.js";
+import { retrieveData } from "./utils/async-storage.js";
 
 export default function App() {
+    const r = retrieveData("theme").then((r) => { return console.log(r); });
     return (
         <View style = {styles.container}>
             <Text>Open up App.js to start working on your app!</Text>

9527507b612ce263db9da288c915c132a0ac3080
Abdul Rehman <abdulr568programming@outlook.com>
2024-12-12 19:12:34 +0000

    test: Updated tests for async code

--- tests/snapshots.test.js
+++ tests/snapshots.test.js
@@ -12,6 +12,21 @@ import Statistics from "../app/screens/statistics/statistics.js";
 import UploadDownloadData from "../app/screens/upload-download-data/upload-download-data.js";
 import ViewGoals from "../app/screens/view-goals/view-goals.js";
 import WorkoutHistory from "../app/screens/workout-history/workout-history.js";
+import { useTheme } from "../app/hooks/useTheme.js";
+
+jest.mock("@react-native-async-storage/async-storage", () => {
+    return {
+        "getItem": jest.fn(() => { return Promise.resolve(null); }),
+        "setItem": jest.fn(() => { return Promise.resolve(); }),
+        "removeItem": jest.fn(() => { return Promise.resolve(); }),
+        "clear": jest.fn(() => { return Promise.resolve(); }),
+        "getAllKeys": jest.fn(() => { return Promise.resolve([]); }),
+        "multiGet": jest.fn(() => { return Promise.resolve([]); }),
+        "multiSet": jest.fn(() => { return Promise.resolve(); }),
+        "multiRemove": jest.fn(() => { return Promise.resolve(); }),
+    };
+});
+
 
 // Set a fixed date and time when running the tests
 const fixedDate = new Date("2024-10-21T00:00:00Z");
@@ -23,9 +38,37 @@ beforeAll(() => {
         }
     };
 });
+
 afterAll(() => {
     global.Date = Date;
 });
+
+beforeEach(() => {
+    useTheme.mockReturnValue({
+        "isReady": true,
+        "colours": {
+            "colour_1": "#FFFFFF", // White
+            "colour_2": "#F1F1F1", // Very Light Grey
+            "colour_3": "#000000", // Black
+            "colour_4": "#060606", // Dark Grey
+            "colour_5": "#DEDEDE", // Light Grey
+            "colour_6": "#F0F0F0", // Very Light Grey
+            "colour_7": "#FF0000", // Red
+            "colour_8": "#D10000", // Strong Red
+            "colour_9": "#E26A00", // Dark Orange
+            "colour_10": "#FB8C00", // Orange
+            "colour_11": "#FFA726", // Vivid Orange
+            "colour_12": "#2296F3", // Vivid Blue
+            "colour_13": "#F1F8FF", // Very Light Blue
+            "colour_14": "#F6F8FA", // Grayish Blue
+            "colour_15": "#FFD700", // Yellow
+        },
+    });
+});
+
+jest.mock("../app/hooks/useTheme.js", () => { return {
+    "useTheme": jest.fn(),
+}; });
 
 test("index.js Test", () => {
     const snapshot = renderer.create(<App />);

738f8b33fcab1923963fda40c109d240f5cc21db
Abdul Rehman <abdulr568programming@outlook.com>
2024-12-13 19:57:27 +0000

    style: Gave footer the correct colours

--- app/constants/colours.js
+++ app/constants/colours.js
@@ -6,6 +6,8 @@ const light_mode = {
     "input_field_background_1": "#DEDEDE",
     "badge_completed": "#FFD700",
     "badge_uncompleted": "#000000",
+    "footer_background": "#D10000",
+    "footer_images": "#060606",
     "colour_1": "#FFFFFF", // White
     "colour_2": "#F1F1F1", // Very Light Grey
     "colour_3": "#000000", // Black
@@ -29,6 +31,8 @@ const dark_mode = {
     "input_field_background_1": "#7F7F7F",
     "badge_completed": "#FFAB00",
     "badge_uncompleted": "#FFFFFF",
+    "footer_background": "#B71C1C",
+    "footer_images": "#F0F0F0",
     "colour_1": "#000000", // Black
     "colour_2": "#0A0A0A", // Very Dark Grey
     "colour_3": "#FFFFFF", // White

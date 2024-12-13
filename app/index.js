50384ab7fcf098bc3cc3d156ec5d4aff4849af3b
Abdul Rehman <abdulr568programming@outlook.com>
2024-12-13 15:15:25 +0000

    fix: Fixed wrong text colour

--- app/index.js
+++ app/index.js
@@ -17,7 +17,7 @@ export default function App() {
 
     return (
         <View style = {[styles.container, { "backgroundColor": colours.colour_1 }]}> 
-            <Text style = {{ "color": colours.colour_15 }}>Open up App.js to start working on your app!</Text>
+            <Text style = {{ "color": colours.colour_3 }}>Open up App.js to start working on your app!</Text>
             <StatusBar style = "auto" />
             <Footer />
         </View>

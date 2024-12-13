3bcc91fdca5ed1aa70be3e54d4e5df3ddf335db3
Abdul Rehman <abdulr568programming@outlook.com>
2024-12-13 16:24:50 +0000

    style: Updated background colour of boxes

--- app/screens/view-goals/view-goals.js
+++ app/screens/view-goals/view-goals.js
@@ -10,13 +10,13 @@ const ViewGoals = () => {
     }
     return (
         <ScrollView style = {{ "backgroundColor": colours.colour_1 }}>
-            <TouchableOpacity style = {{ "backgroundColor": colours.colour_7 }} className = "p-2 h-20 justify-center mt-[5px] w-4/6 items-center self-center mb-5">
+            <TouchableOpacity style = {{ "backgroundColor": "#FF0000" }} className = "p-2 h-20 justify-center mt-[5px] w-4/6 items-center self-center mb-5">
                 <Text className = "font-medium text-base" style = {{ "color": colours.colour_4 }}>Goal 1</Text>
             </TouchableOpacity>
-            <TouchableOpacity style = {{ "backgroundColor": colours.colour_7 }} className = "p-2 h-20 justify-center mt-[5px] w-4/6 items-center self-center mb-5">
+            <TouchableOpacity style = {{ "backgroundColor": "#FF0000" }} className = "p-2 h-20 justify-center mt-[5px] w-4/6 items-center self-center mb-5">
                 <Text className = "font-medium text-base" style = {{ "color": colours.colour_4 }}>Goal 2</Text>
             </TouchableOpacity>
-            <TouchableOpacity style = {{ "backgroundColor": colours.colour_7 }} className = "p-2 h-20 justify-center mt-[5px] w-4/6 items-center self-center mb-5">
+            <TouchableOpacity style = {{ "backgroundColor": "#FF0000" }} className = "p-2 h-20 justify-center mt-[5px] w-4/6 items-center self-center mb-5">
                 <Text className = "font-medium text-base" style = {{ "color": colours.colour_4 }}>Goal 3</Text>
             </TouchableOpacity>
         </ScrollView>

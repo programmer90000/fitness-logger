22dfcecc2606f6b1554b31f33df93e1cd6597c05
Abdul Rehman <abdulr568programming@outlook.com>
2024-12-14 15:51:27 +0000

    test: Updated colours

--- tests/snapshots.test.js
+++ tests/snapshots.test.js
@@ -47,21 +47,20 @@ beforeEach(() => {
     useTheme.mockReturnValue({
         "isReady": true,
         "colours": {
-            "colour_1": "#FFFFFF", // White
-            "colour_2": "#F1F1F1", // Very Light Grey
-            "colour_3": "#000000", // Black
-            "colour_4": "#060606", // Dark Grey
-            "colour_5": "#DEDEDE", // Light Grey
-            "colour_6": "#F0F0F0", // Very Light Grey
-            "colour_7": "#FF0000", // Red
-            "colour_8": "#D10000", // Strong Red
-            "colour_9": "#E26A00", // Dark Orange
-            "colour_10": "#FB8C00", // Orange
-            "colour_11": "#FFA726", // Vivid Orange
-            "colour_12": "#2296F3", // Vivid Blue
-            "colour_13": "#F1F8FF", // Very Light Blue
-            "colour_14": "#F6F8FA", // Grayish Blue
-            "colour_15": "#FFD700", // Yellow
+            "main_background": "#F1F1F1",
+            "button_background_1": "#FF0000",
+            "button_background_2": "#F1F1F1",
+            "button_text_1": "#060606",
+            "text_1": "#060606",
+            "input_field_background_1": "#DEDEDE",
+            "badge_completed": "#FFD700",
+            "badge_uncompleted": "#000000",
+            "footer_background": "#D10000",
+            "footer_images": "#060606",
+            "statistics_title": "#F6F8FA",
+            "statistics_head": "#F1F8FF",
+            "heading_colour_1": "#000000",
+            "heading_colour_2": "#060606",
         },
     });
 });

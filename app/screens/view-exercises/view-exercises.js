7599e3cdba0cdef80cea9491b39877d19a196746
Abdul Rehman <abdulr568programming@outlook.com>
2024-12-26 18:55:38 +0000

    screen: Added edit button next to each exercise

--- app/screens/view-exercises/view-exercises.js
+++ app/screens/view-exercises/view-exercises.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { ScrollView, Text, TouchableOpacity } from "react-native";
 import Realm from "realm";
+import { Ionicons } from "@expo/vector-icons";
 import { useTheme } from "../../hooks/useTheme.js";
 import { colours } from "../../constants/colours.js";
 import { exercises } from "../../../database/realm-database.js";
@@ -39,8 +40,9 @@ const ViewExercise = () => {
                 <Text className = "text-xl text-center mt-5" style = {{ "color": colours.button_text_1 }}>No exercises available</Text>
             ) : (
                 exercisesList.map((exercise) => { return (
-                    <TouchableOpacity key = {exercise.id} className = "p-2.5 h-20 justify-center mt-1.5 w-4/5 self-center mb-1.5" style = {{ "backgroundColor": colours.button_background_1 }}>
+                    <TouchableOpacity key = {exercise.id} className = "flex-row p-2.5 h-20 justify-between items-center mt-1.5 w-4/5 self-center mb-1.5" style = {{ "backgroundColor": colours.button_background_1 }}>
                         <Text className = "text-xl text-center" style = {{ "color": colours.button_text_1 }}>{exercise.name}</Text>
+                        <Ionicons name = "pencil" size = {24} color = {colours.button_icon_1} />
                     </TouchableOpacity>
                 ); })
             )}

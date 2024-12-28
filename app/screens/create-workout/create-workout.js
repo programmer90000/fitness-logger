91a8f8cf63a7079e71754494d2f1e8c846de0ffd
Abdul Rehman <abdulr568programming@outlook.com>
2024-12-28 16:54:47 +0000

    screen: Display created workout presets

--- app/screens/create-workout/create-workout.js
+++ app/screens/create-workout/create-workout.js
@@ -1,19 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ScrollView, Text, TouchableOpacity } from "react-native";
 import { Link } from "expo-router";
+import Realm from "realm";
 import { useTheme } from "../../hooks/useTheme.js";
 import { colours } from "../../constants/colours.js";
+import { workoutPresets } from "../../../database/realm-database.js";
 
 const CreateWorkout = () => {
-    const workoutPresets = [
-        { "name": "Workout Preset 1" },
-        { "name": "Workout Preset 2" },
-        { "name": "Workout Preset 3" },
-        { "name": "Workout Preset 4" },
-        { "name": "Workout Preset 5" },
-    ];
-    
+    const [allWorkoutPresets, setAllWorkoutPresets] = useState([]);
     const { isReady, colours } = useTheme();
+
+    useEffect(() => {
+        if (!isReady) {
+            return;
+        }
+
+        const realm = new Realm({ "schema": [workoutPresets] });
+        const allWorkoutPresets = realm.objects("WorkoutPresets");
+        setAllWorkoutPresets(allWorkoutPresets);
+
+        return () => {
+            realm.close();
+        };
+    }, [isReady]);
 
     if (!isReady) {
         return null;
@@ -27,7 +36,7 @@ const CreateWorkout = () => {
                 </TouchableOpacity>
             </Link>
             <Text className = "text-2xl self-center m-10 mb-5" style = {{ "color": colours.text_1 }}>Workout Presets</Text>
-            {workoutPresets.map((item, index) => { return (
+            {allWorkoutPresets.map((item, index) => { return (
                 <TouchableOpacity key = {index} style = {{ "backgroundColor": colours.button_background_1 }} className = "p-2 mt-[5px] w-4/6 items-center self-center mb-5">
                     <Text className = "font-medium text-base" style = {{ "color": colours.button_text_1 }}>{item.name}</Text>
                 </TouchableOpacity>

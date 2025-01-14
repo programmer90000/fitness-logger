5e1ce15fbf50d9187b4aaf536a75b91df082e8eb
Abdul Rehman <abdulr568programming@outlook.com>
2025-01-14 11:46:00 +0000

    feat: Display average Weight X Reps in Line Chart

--- app/screens/statistics/statistics.js
+++ app/screens/statistics/statistics.js
@@ -14,6 +14,7 @@ const Statistics = () => {
     const [numberOfWeightAndRepsExercises, setNumberOfWeightAndRepsExercises] = useState();
     const [numberOfDistanceAndTimeExercises, setNumberOfDistanceAndTimeExercises] = useState();
     const [averageNumberOfExercisesPerWorkout, setAverageNumberOfExercisesPerWorkout] = useState();
+    const [weightSizeTimesReps, setWeightSizeTimesReps] = useState([]);
 
     useEffect(() => {
         const realm = new Realm({ "schema": [previousWorkouts, exercises, previousWorkoutsExercises] });
@@ -32,6 +33,21 @@ const Statistics = () => {
         previousWorkoutsExercisesArray.forEach((previousWorkout) => {
             const volumeMultipliedByMetrics = previousWorkout.volume * previousWorkout.metrics;
             console.log(volumeMultipliedByMetrics);
+            setWeightSizeTimesReps((prevState) => {
+                const newState = [...prevState, volumeMultipliedByMetrics];
+                if (newState.length > 5) {
+                    const quarterSize = Math.ceil(newState.length / 6);
+                    const averagedArray = [];
+                    for (let i = 0; i < newState.length; i += quarterSize) {
+                        const quarter = newState.slice(i, i + quarterSize);
+                        const average = quarter.reduce((sum, val) => { return sum + val; }, 0) / quarter.length;
+                        averagedArray.push(average);
+                    }
+                    return averagedArray.slice(0, 5);
+                }
+
+                return newState;
+            });
         });
         realm.close();
     }, []);
@@ -57,15 +73,7 @@ const Statistics = () => {
             <Text>Number of exercises measured by Weight and Reps completed: {numberOfWeightAndRepsExercises}</Text>
             <Text>Number of exercises measured by Distance and Time completed: {numberOfDistanceAndTimeExercises}</Text>
             <Text>Average number of exercises per workout: {averageNumberOfExercisesPerWorkout}</Text>
-            <LineChartComponent
-                dataPoints = {[50, 20, 2, 86, 71, 100]} 
-                lineColor = "#000"
-                backgroundGradientFrom = "#FF7F7F"
-                backgroundGradientTo = "#DC0000"
-                labelColor = "#000"
-                dotColor = "#000"
-                decimalPlaces = {2}
-            />
+            <LineChartComponent dataPoints = {weightSizeTimesReps} lineColor = "#000" backgroundGradientFrom = "#FF7F7F" backgroundGradientTo = "#DC0000" labelColor = "#000" dotColor = "#000" decimalPlaces = {2} />
 
         </View>
     );

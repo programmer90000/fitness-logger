24f8463981e929f7a193082141647463db9d8147
Abdul Rehman <abdulr568programming@outlook.com>
2025-01-15 14:16:18 +0000

    feat: Only display data for reps exercises in chart

--- app/screens/statistics/statistics.js
+++ app/screens/statistics/statistics.js
@@ -31,22 +31,24 @@ const Statistics = () => {
         setAverageNumberOfExercisesPerWorkout(realm.objects("PreviousWorkouts").length > 0 ? totalExercises / realm.objects("PreviousWorkouts").length : 0);
         const previousWorkoutsExercisesArray = realm.objects("PreviousWorkoutsExercises");
         previousWorkoutsExercisesArray.forEach((previousWorkout) => {
-            const volumeMultipliedByMetrics = previousWorkout.volume * previousWorkout.metrics;
-            setWeightSizeTimesReps((prevState) => {
-                const newState = [...prevState, volumeMultipliedByMetrics];
-                if (newState.length > 5) {
-                    const quarterSize = Math.ceil(newState.length / 6);
-                    const averagedArray = [];
-                    for (let i = 0; i < newState.length; i += quarterSize) {
-                        const quarter = newState.slice(i, i + quarterSize);
-                        const average = quarter.reduce((sum, val) => { return sum + val; }, 0) / quarter.length;
-                        averagedArray.push(average);
+            if (previousWorkout.exercises.type === "reps") {
+                const volume = previousWorkout.volume;
+                setWeightSizeTimesReps((prevState) => {
+                    const newState = [...prevState, volume];
+                    if (newState.length > 5) {
+                        const quarterSize = Math.ceil(newState.length / 6);
+                        const averagedArray = [];
+                        for (let i = 0; i < newState.length; i += quarterSize) {
+                            const quarter = newState.slice(i, i + quarterSize);
+                            const average = quarter.reduce((sum, val) => { return sum + val; }, 0) / quarter.length;
+                            averagedArray.push(average);
+                        }
+                        return averagedArray.slice(0, 5);
                     }
-                    return averagedArray.slice(0, 5);
-                }
 
-                return newState;
-            });
+                    return newState;
+                });
+            }
         });
         realm.close();
     }, []);
@@ -72,16 +74,8 @@ const Statistics = () => {
             <Text>Number of exercises measured by Weight and Reps completed: {numberOfWeightAndRepsExercises}</Text>
             <Text>Number of exercises measured by Distance and Time completed: {numberOfDistanceAndTimeExercises}</Text>
             <Text>Average number of exercises per workout: {averageNumberOfExercisesPerWorkout}</Text>
-            <Text>Average Weight X Reps over time:</Text>
-            <LineChartComponent
-                dataPoints = {weightSizeTimesReps} 
-                lineColor = "#000"
-                backgroundGradientFrom = "#FF7F7F"
-                backgroundGradientTo = "#DC0000"
-                labelColor = "#000"
-                dotColor = "#000"
-                decimalPlaces = {2}
-            />
+            <Text>Average Reps Exercises over time:</Text>
+            <LineChartComponent dataPoints = {weightSizeTimesReps} lineColor = "#000" backgroundGradientFrom = "#FF7F7F" backgroundGradientTo = "#DC0000" labelColor = "#000" dotColor = "#000" decimalPlaces = {2} />
 
         </View>
     );

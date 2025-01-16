2e11962b4b909cfd70a949d11a92f08808ee6be3
Abdul Rehman <abdulr568programming@outlook.com>
2025-01-16 13:04:00 +0000

    revert: Revert to commit 6b224

--- app/screens/statistics/statistics.js
+++ app/screens/statistics/statistics.js
@@ -31,24 +31,23 @@ const Statistics = () => {
         setAverageNumberOfExercisesPerWorkout(realm.objects("PreviousWorkouts").length > 0 ? totalExercises / realm.objects("PreviousWorkouts").length : 0);
         const previousWorkoutsExercisesArray = realm.objects("PreviousWorkoutsExercises");
         previousWorkoutsExercisesArray.forEach((previousWorkout) => {
-            if (previousWorkout.exercises.type === "reps") {
-                const volume = previousWorkout.volume;
-                setWeightSizeTimesReps((prevState) => {
-                    const newState = [...prevState, volume];
-                    if (newState.length > 5) {
-                        const quarterSize = Math.ceil(newState.length / 6);
-                        const averagedArray = [];
-                        for (let i = 0; i < newState.length; i += quarterSize) {
-                            const quarter = newState.slice(i, i + quarterSize);
-                            const average = quarter.reduce((sum, val) => { return sum + val; }, 0) / quarter.length;
-                            averagedArray.push(average);
-                        }
-                        return averagedArray.slice(0, 5);
+            const volumeMultipliedByMetrics = previousWorkout.volume * previousWorkout.metrics;
+            console.log(volumeMultipliedByMetrics);
+            setWeightSizeTimesReps((prevState) => {
+                const newState = [...prevState, volumeMultipliedByMetrics];
+                if (newState.length > 5) {
+                    const quarterSize = Math.ceil(newState.length / 6);
+                    const averagedArray = [];
+                    for (let i = 0; i < newState.length; i += quarterSize) {
+                        const quarter = newState.slice(i, i + quarterSize);
+                        const average = quarter.reduce((sum, val) => { return sum + val; }, 0) / quarter.length;
+                        averagedArray.push(average);
                     }
+                    return averagedArray.slice(0, 5);
+                }
 
-                    return newState;
-                });
-            }
+                return newState;
+            });
         });
         realm.close();
     }, []);
@@ -74,8 +73,15 @@ const Statistics = () => {
             <Text>Number of exercises measured by Weight and Reps completed: {numberOfWeightAndRepsExercises}</Text>
             <Text>Number of exercises measured by Distance and Time completed: {numberOfDistanceAndTimeExercises}</Text>
             <Text>Average number of exercises per workout: {averageNumberOfExercisesPerWorkout}</Text>
-            <Text>Average Reps Exercises over time:</Text>
-            <LineChartComponent dataPoints = {weightSizeTimesReps} lineColor = "#000" backgroundGradientFrom = "#FF7F7F" backgroundGradientTo = "#DC0000" labelColor = "#000" dotColor = "#000" decimalPlaces = {2} />
+            <LineChartComponent
+                dataPoints = {[50, 20, 2, 86, 71, 100]} 
+                lineColor = "#000"
+                backgroundGradientFrom = "#FF7F7F"
+                backgroundGradientTo = "#DC0000"
+                labelColor = "#000"
+                dotColor = "#000"
+                decimalPlaces = {2}
+            />
 
         </View>
     );

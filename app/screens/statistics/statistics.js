c71873d52577bd46bc4038c16d4840e31a83afd5
Abdul Rehman <abdulr568programming@outlook.com>
2025-01-16 13:42:56 +0000

    feat: Display data for Distance And Time exercises in chart

--- app/screens/statistics/statistics.js
+++ app/screens/statistics/statistics.js
@@ -15,6 +15,7 @@ const Statistics = () => {
     const [numberOfDistanceAndTimeExercises, setNumberOfDistanceAndTimeExercises] = useState();
     const [averageNumberOfExercisesPerWorkout, setAverageNumberOfExercisesPerWorkout] = useState();
     const [weightSizeTimesReps, setWeightSizeTimesReps] = useState([]);
+    const [distanceTimesTime, setDistanceTimesTime] = useState([]);
 
     useEffect(() => {
         const realm = new Realm({ "schema": [previousWorkouts, exercises, previousWorkoutsExercises] });
@@ -48,6 +49,24 @@ const Statistics = () => {
 
                     return newState;
                 });
+            } else if (previousWorkout.exercises.type === "distanceAndTime") {
+                const volumeMultipliedByMetrics = previousWorkout.volume * previousWorkout.metrics;
+                setDistanceTimesTime((prevState) => {
+                    const newState = [...prevState, volumeMultipliedByMetrics];
+                    if (newState.length > 5) {
+                        const quarterSize = Math.ceil(newState.length / 6);
+                        const averagedArray = [];
+                        for (let i = 0; i < newState.length; i += quarterSize) {
+                            const quarter = newState.slice(i, i + quarterSize);
+                            const average = quarter.reduce((sum, val) => { return sum + val; }, 0) / quarter.length;
+                            averagedArray.push(average);
+                        }
+                        return averagedArray.slice(0, 5);
+                    }
+
+                    return newState;
+                });
+                
             }
         });
         realm.close();
@@ -75,6 +94,7 @@ const Statistics = () => {
             <Text>Number of exercises measured by Distance and Time completed: {numberOfDistanceAndTimeExercises}</Text>
             <Text>Average number of exercises per workout: {averageNumberOfExercisesPerWorkout}</Text>
             <LineChartComponent dataPoints = {weightSizeTimesReps} lineColor = "#000" backgroundGradientFrom = "#FF7F7F" backgroundGradientTo = "#DC0000" labelColor = "#000" dotColor = "#000" decimalPlaces = {2} />
+            <LineChartComponent dataPoints = {distanceTimesTime} lineColor = "#000" backgroundGradientFrom = "#FF7F7F" backgroundGradientTo = "#DC0000" labelColor = "#000" dotColor = "#000" decimalPlaces = {2} />
 
         </View>
     );

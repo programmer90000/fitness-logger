eb4c514ec7ef337d498bc710e748f732d6a9284a
Abdul Rehman <abdulr568programming@outlook.com>
2025-02-26 17:25:15 +0000

    optimize: Moved @react-native-async-storage/async-storage mock into jest.setup.js

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,14 @@
 global.__DEV__ = true;
+
+jest.mock("@react-native-async-storage/async-storage", () => {
+    return {
+        "getItem": jest.fn(() => { return Promise.resolve(null); }),
+        "setItem": jest.fn(() => { return Promise.resolve(); }),
+        "removeItem": jest.fn(() => { return Promise.resolve(); }),
+        "clear": jest.fn(() => { return Promise.resolve(); }),
+        "getAllKeys": jest.fn(() => { return Promise.resolve([]); }),
+        "multiGet": jest.fn(() => { return Promise.resolve([]); }),
+        "multiSet": jest.fn(() => { return Promise.resolve(); }),
+        "multiRemove": jest.fn(() => { return Promise.resolve(); }),
+    };
+});
--- tests/snapshots.test.js
+++ tests/snapshots.test.js
@@ -119,19 +119,6 @@ jest.mock("realm", () => {
     return jest.fn(() => { return mockRealm; });
 });
 
-jest.mock("@react-native-async-storage/async-storage", () => {
-    return {
-        "getItem": jest.fn(() => { return Promise.resolve(null); }),
-        "setItem": jest.fn(() => { return Promise.resolve(); }),
-        "removeItem": jest.fn(() => { return Promise.resolve(); }),
-        "clear": jest.fn(() => { return Promise.resolve(); }),
-        "getAllKeys": jest.fn(() => { return Promise.resolve([]); }),
-        "multiGet": jest.fn(() => { return Promise.resolve([]); }),
-        "multiSet": jest.fn(() => { return Promise.resolve(); }),
-        "multiRemove": jest.fn(() => { return Promise.resolve(); }),
-    };
-});
-
 jest.mock("@react-navigation/native", () => { return {
     ...jest.requireActual("@react-navigation/native"),
     "useNavigation": () => { return {

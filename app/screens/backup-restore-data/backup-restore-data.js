7040077c06d8400a57b4fef9de36170bb0dfd9b0
Abdul Rehman <abdulr568programming@outlook.com>
2025-04-09 15:31:30 +0100

    revert: Revert to commit 6260e2

--- app/screens/backup-restore-data/backup-restore-data.js
+++ app/screens/backup-restore-data/backup-restore-data.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import Realm from "realm";
 import * as FileSystem from "expo-file-system";
 import * as DocumentPicker from "expo-document-picker";
-import Share from "react-native-share";
+import * as Sharing from "expo-sharing";
 import { workoutPresets, exercises, workoutPresetsExercises, previousWorkouts, previousWorkoutsExercises, goals, badges } from "../../../database/realm-database.js";
 import { useTheme } from "../../hooks/useTheme.js";
 import { colours } from "../../constants/colours.js";
@@ -232,10 +232,12 @@ const BackupRestoreData = () => {
                 "encoding": FileSystem.EncodingType.UTF8,
             });
             
-            await Share.open({
-                "url": `file://${fileUri}`,
-                "title": "Share File",
-            });
+            if (!(await Sharing.isAvailableAsync())) {
+                Alert.alert("Error", "Sharing is not available on this device.");
+                return;
+            }
+
+            await Sharing.shareAsync(fileUri);
         } catch (error) {
             console.error("Error creating JSON data:", error);
             throw error;

6a9ec85922fd02e575899044283a97d321ce4016
Abdul Rehman <abdulr568programming@outlook.com>
2025-04-14 19:19:23 +0100

    test: Mock expo-splash-screen

---
+++ __mocks__/expo-splash-screen.js
@@ -0,0 +1,6 @@
+export const preventAutoHideAsync = jest.fn(() => { return Promise.resolve(); });
+export const hideAsync = jest.fn(() => { return Promise.resolve(); });
+export default {
+    "preventAutoHideAsync": jest.fn(() => { return Promise.resolve(); }),
+    "hideAsync": jest.fn(() => { return Promise.resolve(); }),
+};

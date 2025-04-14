1ff5015384a9bc084569a591cfefd1732f0d4135
Abdul Rehman <abdulr568programming@outlook.com>
2025-04-14 19:17:49 +0100

    test: Mock multiple Vector Icon

---
+++ __mocks__/@expo/vector-icons/index.js
@@ -0,0 +1,5 @@
+export const createIconSet = () => { return "Icon-Mock"; };
+export const Entypo = "Entypo-Mock";
+export const FontAwesome5 = "FontAwesome5-Mock";
+export const AntDesign = "AntDesign-Mock";
+export const MaterialCommunityIcons = "MaterialCommunityIcons-Mock";

48c5a6ceef488ae89ad8695245e8b9c7fe41d2c6
Abdul Rehman <abdulr568programming@outlook.com>
2025-04-15 14:01:18 +0100

    optimize: Replaced History icon with custom icon

--- app/components/Footer/Footer.js
+++ app/components/Footer/Footer.js
@@ -2,8 +2,7 @@ import { View } from "react-native";
 import { styles } from "./style.js";
 import { Link } from "expo-router";
 
-import { HouseIcon } from "../icons/icons.js";
-import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
+import { HouseIcon, ClockIcon } from "../icons/icons.js";
 import AntDesign from "@expo/vector-icons/AntDesign";
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 import Feather from "@expo/vector-icons/Feather";
@@ -12,7 +11,7 @@ export default function Footer() {
     return (
         <View style = {styles.container}>
             <Link href = "/" style = {{ "width": 30, "height": 30 }}><HouseIcon size = {30} color = "black" /></Link>
-            <Link href = "/"><FontAwesome5 name = "history" size = {24} color = "black" /></Link>
+            <Link href = "/" style = {{ "width": 30, "height": 30 }}><ClockIcon size = {30} color = "black" /></Link>
             <Link href = "/"><AntDesign name = "plussquare" size = {24} color = "black" /></Link>
             <Link href = "/"><MaterialCommunityIcons name = "chart-line" size = {24} color = "black" /></Link>            
             <Link href = "/"><Feather name = "settings" size = {24} color = "black" /></Link>
--- app/components/icons/icons.js
+++ app/components/icons/icons.js
@@ -1,4 +1,4 @@
-import Svg, { Path } from "react-native-svg";
+import Svg, { Path, Circle, Line } from "react-native-svg";
 
 const HouseIcon = ({ size = 30, color = "black" }) => {
     return (
@@ -8,4 +8,23 @@ const HouseIcon = ({ size = 30, color = "black" }) => {
     );
 };
 
-export { HouseIcon };
+
+const ClockIcon = ({ size = 30, strokeWidth = 3, colour = "black" }) => {
+    const center = size / 2;
+    const handLength = size / 3;
+    const minuteHandAngle = 90;
+    const minuteHandAngleInRadians = (minuteHandAngle * Math.PI) / 180;
+
+    const minuteHandX = (center + handLength) * Math.sin(minuteHandAngleInRadians);
+    const minuteHandY = center - (handLength * Math.cos(minuteHandAngleInRadians));
+
+    return (
+        <Svg width = {size} height = {size} viewBox = {`0 0 ${size} ${size}`}>
+            <Circle cx = {center} cy = {center} r = {center - strokeWidth} stroke = {colour} strokeWidth = {strokeWidth} fill = "none" />
+            <Line x1 = {center} y1 = {center} x2 = {center} y2 = {center - handLength} stroke = {colour} strokeWidth = {strokeWidth} />            
+            <Line x1 = {center} y1 = {center} x2 = {minuteHandX} y2 = {minuteHandY} stroke = {colour} strokeWidth = {strokeWidth} />
+        </Svg>
+    );
+};
+
+export { HouseIcon, ClockIcon };

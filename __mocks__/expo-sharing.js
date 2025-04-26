c8d127089b8d091fc6432c17fb763e4a0b5b95bd
Abdul Rehman <abdulr568programming@outlook.com>
2025-04-26 18:21:11 +0100

    optimize: Removed expo-sharing mock

--- __mocks__/expo-sharing.js
+++
@@ -1,4 +0,0 @@
-jest.mock("expo-sharing", () => { return {
-    "isAvailableAsync": jest.fn(() => { return Promise.resolve(true); }),
-    "shareAsync": jest.fn(() => { return Promise.resolve(); }),
-}; });

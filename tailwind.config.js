145efad6575996501d8aac8f4e2492f018e13c0b
Abdul Rehman <abdulr568programming@outlook.com>
2025-05-18 11:37:35 +0100

    optimize: Installed NativeWind

---
+++ tailwind.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    // Add any other directories or files which use Tailwind CSS
+    content: ["./src/**/*.{js,jsx}"],
+    // Require any Tailwind CSS plugins I want to use, such as require("@tailwindcss/forms")
+    plugins: [],
+}

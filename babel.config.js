8a00fad20ccf9485868fcc4f976085eeb2e169d2
Abdul Rehman <abdulr568programming@outlook.com>
2025-05-19 15:18:20 +0100

    fix: Fixed error running app

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,6 @@ module.exports = function(api) {
     api.cache(true);
     return {
         "presets": ["module:metro-react-native-babel-preset"],
-        "plugins": ["nativewind/babel"],
+        "plugins": [ ["@babel/plugin-transform-class-properties", { "loose": false }], ["@babel/plugin-transform-private-methods", { "loose": false }], ["@babel/plugin-transform-private-property-in-object", { "loose": false }], "nativewind/babel" ],
     };
 };

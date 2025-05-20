2e528ca60e0bf22bc2606b4f5e8c45e03d82b66f
Abdul Rehman <abdulr568programming@outlook.com>
2025-05-20 09:16:19 +0100

    optimize: Created async-storage file

---
+++ src/utils/async-storage.js
@@ -0,0 +1,25 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const storeData = async (key, newValue, consoleLog) => {
+    try {
+        await AsyncStorage.setItem(key, newValue);
+
+        if (consoleLog) {
+            const storedValue = await AsyncStorage.getItem(key);
+            console.log(`Retrieved ${key}: ${storedValue}`);
+        }
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+const retrieveData = async (key) => {
+    try {
+        const jsonValue = await AsyncStorage.getItem(key);
+        return jsonValue;
+    } catch (error) {
+        console.log(`Error retrieving value: ${error}`);
+    }
+};
+
+export { storeData, retrieveData };

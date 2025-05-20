222f8818ee5cb38c670bace6fa4ded970c721c58
Abdul Rehman <abdulr568programming@outlook.com>
2025-05-20 10:05:27 +0100

    optimize: Created line chart component

---
+++ src/components/line-chart/line-chart.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { View, Dimensions } from "react-native";
+import { LineChart } from "react-native-chart-kit";
+
+const LineChartComponent = ({ labels, dataPoints, lineColor, backgroundGradientFrom, backgroundGradientTo, labelColor, dotColor, decimalPlaces = 2 }) => {
+    const data = {
+        labels,
+        "datasets": [
+            {
+                "data": dataPoints,
+                "color": (opacity = 1) => { return lineColor.replace("1)", `${opacity})`); },
+                "strokeWidth": 2,
+            },
+        ],
+    };
+
+    const chartConfig = {
+        backgroundGradientFrom,
+        backgroundGradientTo,
+        decimalPlaces,
+        "color": (opacity = 1) => { return labelColor.replace("1)", `${opacity})`); },
+        "labelColor": (opacity = 1) => { return labelColor.replace("1)", `${opacity})`); },
+        "style": { "borderRadius": 16 },
+        "propsForDots": {
+            "r": "6",
+            "strokeWidth": "2",
+            "stroke": dotColor,
+        },
+    };
+
+    const screenWidth = Dimensions.get("window").width;
+
+    return (
+        <View className = "flex justify-center items-center">
+            <LineChart data = {data} width = {screenWidth * 0.9} height = {220} chartConfig = {chartConfig} bezier fromZero className = "my-2 rounded-2xl" />
+        </View>
+    );
+};
+
+export { LineChartComponent };

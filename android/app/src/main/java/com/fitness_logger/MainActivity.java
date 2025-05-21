c17d00e2f8d1b698d6bdf33db289d5bc4f3a73e5
Abdul Rehman <127865680+programmer90000@users.noreply.github.com>
2025-05-21 16:31:58 +0100

    optimize: Changed android/ dir to use java instead of kotlin

---
+++ android/app/src/main/java/com/fitness_logger/MainActivity.java
@@ -0,0 +1,28 @@
+package com.fitness_logger;
+
+import android.os.Bundle;
+
+import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
+import com.facebook.react.defaults.DefaultReactActivityDelegate;
+import com.zoontek.rnbootsplash.RNBootSplash;
+
+public class MainActivity extends ReactActivity {
+
+  @Override
+  protected String getMainComponentName() {
+    return "fitness_logger";
+  }
+
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new DefaultReactActivityDelegate(this, getMainComponentName(), DefaultNewArchitectureEntryPoint.getFabricEnabled());
+  }
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    RNBootSplash.init(this, R.style.BootTheme);
+    super.onCreate(savedInstanceState);
+  }
+}

5cc946accef3a7df6fc473028cad65bd7bf0b406
Abdul Rehman <abdulr568programming@outlook.com>
2025-05-22 13:40:25 +0100

    fix: Corrected MainActivity.java

--- android/app/src/main/java/com/fitness_logger/MainActivity.java
+++ android/app/src/main/java/com/fitness_logger/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends ReactActivity {
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
     RNBootSplash.init(this, R.style.BootTheme);
-    super.onCreate(savedInstanceState);
   }
 }

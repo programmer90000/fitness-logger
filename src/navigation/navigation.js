4b9cae253fa7a75d69afc49af46f091ad7a7e9b1
Abdul Rehman <abdulr568programming@outlook.com>
2025-05-22 15:05:46 +0100

    optimize: Made stack navigator alongside drawer navigator

--- src/navigation/navigation.js
+++ src/navigation/navigation.js
@@ -1,8 +1,18 @@
 import React from "react";
 import { createDrawerNavigator } from "@react-navigation/drawer";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import App from "../screens/home/App.js";
 
 const Drawer = createDrawerNavigator();
+const Stack = createNativeStackNavigator();
+
+function Screens() {
+    return (
+        <Stack.Navigator screenOptions = {{ "headerShown": false }}>
+            <Stack.Screen name = "Home" component = {App} />
+        </Stack.Navigator>
+    );
+}
 
 export default function Navigation() {
     return (

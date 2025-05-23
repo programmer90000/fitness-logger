832ecba6dd916eae0a59236c5db09ea27743a0dd
Abdul Rehman <abdulr568programming@outlook.com>
2025-05-23 14:46:07 +0100

    fix: Corrected footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,15 +1,17 @@
-import { View, Link } from "react-native";
+import { View, TouchableOpacity } from "react-native";
 import { HomeIcon, CalendarDaysIcon, PlusCircleIcon, ChartBarIcon, Cog6ToothIcon } from "react-native-heroicons/solid";
+import { useNavigation } from "@react-navigation/native";
 import { colours } from "../../constants/colours.js";
 
 export default function Footer() {
+    const navigation = useNavigation();
     return (
         <View style = {{ "backgroundColor": colours.footer_background }} className = "absolute bottom-0 w-full flex-row items-center justify-evenly py-[1%]">
-            <Link href = "/"><HomeIcon size = {24} color = {colours.footer_images} /></Link>
-            <Link href = "/"><CalendarDaysIcon size = {24} color = {colours.footer_images} /></Link>
-            <Link href = "/"><PlusCircleIcon size = {24} color = {colours.footer_images} /></Link>
-            <Link href = "/"><ChartBarIcon size = {24} color = {colours.footer_images} /></Link>
-            <Link href = "/"><Cog6ToothIcon size = {24} color = {colours.footer_images} /></Link>
+            <TouchableOpacity onPress = {() => { return navigation.navigate("Home"); }}><HomeIcon size = {24} color = {colours.footer_images} /></TouchableOpacity>
+            <TouchableOpacity onPress = {() => { return navigation.navigate("Home"); }}><CalendarDaysIcon size = {24} color = {colours.footer_images} /></TouchableOpacity>
+            <TouchableOpacity onPress = {() => { return navigation.navigate("Home"); }}><PlusCircleIcon size = {24} color = {colours.footer_images} /></TouchableOpacity>
+            <TouchableOpacity onPress = {() => { return navigation.navigate("Home"); }}><ChartBarIcon size = {24} color = {colours.footer_images} /></TouchableOpacity>
+            <TouchableOpacity onPress = {() => { return navigation.navigate("Home"); }}><Cog6ToothIcon size = {24} color = {colours.footer_images} /></TouchableOpacity>
         </View>
     );
 }

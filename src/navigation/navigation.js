05b2c8223446b2e0772df074e884fd2e02284b15
Abdul Rehman <abdulr568programming@outlook.com>
2025-05-26 20:05:44 +0100

    optimize: Added style for header

--- src/navigation/navigation.js
+++ src/navigation/navigation.js
@@ -16,7 +16,7 @@ function Screens() {
 
 export default function Navigation() {
     return (
-        <Drawer.Navigator>
+        <Drawer.Navigator screenOptions = {{ "headerStyle": { "backgroundColor": "#FF0000" }, "drawerStyle": { "backgroundColor": "#FF0000", "width": 240 }, "drawerContentStyle": { "backgroundColor": "transparent" }, "drawerActiveTintColor": "#F1F1F1", "drawerInactiveTintColor": "#060606", "drawerActiveBackgroundColor": "#FF4242" }}>
             <Drawer.Screen name = "Home" component = {Home} />
         </Drawer.Navigator>
     );

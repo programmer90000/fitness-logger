0d75f36e06780d6ef35ba9c6669962447f54a865
Abdul Rehman <abdulr568programming@outlook.com>
2025-05-27 16:01:27 +0100

    optimize: Added FAQ data to home screen

--- src/screens/home/home.js
+++ src/screens/home/home.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { ScrollView, Text, StatusBar } from "react-native";
 import BootSplash from "react-native-bootsplash";
 import { useTheme } from "../../hooks/use-theme.js";
+import Accordion from "../../components/accordion/accordion.js";
 
 function Home() {
     const { isReady, colours } = useTheme();
@@ -21,10 +22,16 @@ function Home() {
         return null;
     }
 
+const faqData = [
+        { "title": "What is React Native?", "content": "React Native is a framework for building mobile apps." },
+        { "title": "How does it work?", "content": "It uses JavaScript and React to build native mobile apps." },
+        { "title": "Is it easy to learn?", "content": "Yes, especially if you know JavaScript and React." },
+    ];
+
     return (
         <ScrollView className = "flex-1" style = {{ "backgroundColor": colours.main_background }} contentContainerStyle = {{ "alignItems": "center" }}>
             <StatusBar barStyle = "light-content" backgroundColor = "#ff0000" />
-            <Text>Temp</Text>
+            <Accordion data = {faqData} style = "mt-3" />
         </ScrollView>
     );
 }

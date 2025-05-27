16742a6c224bc35825266833b1f7f57650bf74f2
Abdul Rehman <abdulr568programming@outlook.com>
2025-05-27 16:03:25 +0100

    optimize: Added benefits of using fitness logger to home screen

--- src/screens/home/home.js
+++ src/screens/home/home.js
@@ -22,16 +22,25 @@ function Home() {
         return null;
     }
 
-const faqData = [
+    const faqData = [
         { "title": "What is React Native?", "content": "React Native is a framework for building mobile apps." },
         { "title": "How does it work?", "content": "It uses JavaScript and React to build native mobile apps." },
         { "title": "Is it easy to learn?", "content": "Yes, especially if you know JavaScript and React." },
     ];
 
+    const benefitsOfUsingFitnessLogger = [
+        { "title": "Enhanced Awareness of Activity Levels", "content": "This fitness tracker app help you gain a better understanding of your time spent being active. This insight can reveal gaps in your routine and help you to make healthier choices, ensuring you stay on track toward your fitness goals." },
+        { "title": "Personalized Goal Setting", "content": "With this fitness tracker app, you can set realistic and achievable fitness goals tailored to your current fitness level. Whether it’s completing a number of push-ups or increasing your weekly exercise minutes, these apps guide you with measurable objectives that help you progress steadily." },
+        { "title": "Building Discipline", "content": "Fitness tracker apps promote discipline by encouraging consistent effort over time. Progress charts keep you accountable, helping you establish a routine. Over time, this consistency strengthens your willpower and instills a habit of prioritizing physical activity." },
+        { "title": "Health Insights", "content": "Fitness trackers store your health data over time, providing valuable insights into trends in your fitness level. These records can be shared with healthcare providers to support better-informed medical decisions." },
+        { "title": "Progress Visualization", "content": "The visual representation of your progress—graphs, charts, and milestones—can be a powerful motivator. Seeing how far you’ve come reinforces positive behavior and encourages you to keep pushing toward your fitness goals.Improving fitness through a fitness tracker app is not just about physical health; it’s about building habits, enhancing awareness, and creating a sustainable lifestyle change." },
+    ];
+
     return (
         <ScrollView className = "flex-1" style = {{ "backgroundColor": colours.main_background }} contentContainerStyle = {{ "alignItems": "center" }}>
             <StatusBar barStyle = "light-content" backgroundColor = "#ff0000" />
-            <Accordion data = {faqData} style = "mt-3" />
+            <Accordion data={faqData} style="mt-3" />
+            <Accordion data = {benefitsOfUsingFitnessLogger} style = "mb-10" />
         </ScrollView>
     );
 }
